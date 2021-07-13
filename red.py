65e67e9f1620d12fb8352821c17af79154732c74
Kakeru Abe <41567648+net0105net@users.noreply.github.com>
2021-07-14 00:10:38 +0900

    Update red.py
    Add import sequence, and arrange code.

--- red.py
+++ red.py
@@ -1,12 +1,43 @@
-from __future__ import print_function
+from __future__ import print_function  ## for Python 2.XX
 import nltk
-title = "price of Banana"
-#value = {'Jan':100, 'Feb':120, 'Mar':140, 'Apr':110, 'May':100, 'Jun':150}
-value = [100, 120, 140, 110, 100, 100]
-month = ["January", "February", "March", "April", "May", "June"]
-sentence = "Price of banana"
+import csv
 
+## import CSV file.
+## Please keep the CSV on same directory, or change path. ----
+with open('sample.csv', 'r') as csvFile:
+    reader = csv.reader(csvFile)
+    line = [row for row in reader]
 
+## -----------------------------------------------------------
+
+#title = "price of Banana"
+#value = [100, 120, 140, 110, 100, 100]
+#month = ["January", "February", "March", "April", "May", "June"]
+
+## Store data of month and value into List.----------------
+value = []
+month = []
+for i in range(len(line)):
+    if i == 0: ## First line should be title.
+        sentence = line[i][0]
+    elif line[i][0] == '': ## if there no word, continue.
+        continue;
+    else:
+        month.append(line[i][0])
+        value.append(int(line[i][1]))
+
+## --------------------------------------------------------
+        
+#sentence = "Price of banana"
+
+## Debug ---------
+#print(sentence)
+#print(month)
+#print(value)
+
+## ---------------
+
+## Output sequenses -----------------------------------------------------------------------
 for i in range(len(value)-1):
     
     sub = abs(value[i] - value[i+1])
@@ -19,5 +50,6 @@ for i in range(len(value)-1):
         print("increase from " + month[i] + " to " + month[i+1] + " by " + str(sub) + ".")
     else:
         print("remain stable from " + month[i] + " to " + month[i+1] + ".")
+
 
 
